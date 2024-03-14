ed3b49b41b284ed84671dda5969ecaff0f75fbe9
Daniel <danielpsilva2704@gmail.com>
2024-03-14 09:51:07 -0300

    atividade1

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,29 @@
+//Atividade 1
+
+/*2 – Faça a declaração de 2 variáveis numéricas para cada
+operador e guarde o resultado da operação em uma terceira
+variável;
+3 – Para cada resultado obtido, imprima o texto “Resultado da
+{operação} entre {num_A} e {num_B}”*/
+
+
+let somA = 5;
+let somB = 3;
+let soma = somA + somB;
+console.log('Resultado da soma entre 5 e 3 é:', soma);
+
+let subA = 10;
+let subB = 7;
+let sub = subA - subB;
+console.log('Resuldado da subtração entre 10 e 7 é:', sub);
+
+let multA = 5;
+let multB = 8;
+let mult = multA * multB;
+console.log('Resultado da multiplicação entre 5 e 8 é:', mult);
+
+let divA = 100;
+let divB = 25;
+let div = divA/divB;
+console.log('Resultado da divisão de 100 pra 25 é:', div);
+
